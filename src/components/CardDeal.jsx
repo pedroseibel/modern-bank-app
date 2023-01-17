1743f26e0b14ff3c5635f34bf9b52e42123bd7df
Pedro Seibel <pedrosbl@icloud.com>
2023-01-17 19:31:15 -0300

    Fixing space between words

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -6,7 +6,7 @@ const CardDeal = () => {
   return (
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Find a better card deal<br className="sm:block hidden"/>in few easy steps.</h2>
+        <h2 className={styles.heading2}>Find a better card deal<br className="sm:block hidden"/> in few easy steps.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor. Aliquet ultrices ac, ametau.</p>
         <Button styles="mt-10"/>
       </div>
