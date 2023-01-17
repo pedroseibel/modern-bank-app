35e45c5e267ff758e95e2c9cd256c3dab79542b3
Pedro Seibel <pedrosbl@icloud.com>
2023-01-16 22:17:28 -0300

    Footer import fix

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import Business from './Business';
 import Clients from './Clients';
 import CTA from './CTA';
 import Stats from './Stats';
-import Footer from './footer';
+import Footer from './Footer';
 import Testimonials from './Testimonials';
 import Hero from './Hero';
 
